3d71d09f3ac2b3884bbcc6d99c2c0f984f277a89
Marin Godechot <marin.godechot@gmail.com>
2025-09-17 16:12:28 -0400

    remove unused import

--- src/mastra/agents/genai-recipe-agent.ts
+++ src/mastra/agents/genai-recipe-agent.ts
@@ -2,7 +2,6 @@ import { openai } from "@ai-sdk/openai";
 import { Agent } from "@mastra/core/agent";
 import { Memory } from "@mastra/memory";
 import { LibSQLStore } from "@mastra/libsql";
-import { weatherTool } from "../tools/weather-tool";
 
 import { createOllama } from "ollama-ai-provider";
 import { bestPracticeTool } from "../tools/best-practice-tool";
